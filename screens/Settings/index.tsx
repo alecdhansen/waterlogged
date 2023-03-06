823836dd8d2af8ff7b1347ab489db60745c6bfda
Alec Hansen <alecdhansen@gmail.com>
2023-03-06 14:27:46 -0500

    add Settings component

---
+++ screens/Settings/index.tsx
@@ -0,0 +1,7 @@
+import { View } from "../../components/Themed";
+import { styles } from "./style";
+
+const Settings = () => {
+  return <View style={styles.container} />;
+};
+export default Settings;

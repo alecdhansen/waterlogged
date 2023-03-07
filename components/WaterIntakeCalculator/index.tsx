dec81735d06f96cf5184f1ac70ac70d754830768
Alec Hansen <alecdhansen@gmail.com>
2023-03-06 21:28:39 -0500

    add waterintakecalculator

---
+++ components/WaterIntakeCalculator/index.tsx
@@ -0,0 +1,191 @@
+import React, { useEffect, useState } from "react";
+import { TextInput, Button, DatePickerIOSComponent } from "react-native";
+import { View, Text } from "../Themed";
+import { DatePickerOptions } from "@react-native-community/datetimepicker";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+type WaterData = {
+  date: string;
+  weight: string;
+  height: string;
+  age: string;
+  dailyIntake: string;
+  drankToday: string;
+};
+
+const WaterIntakeCalculator = () => {
+  const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+  const [weight, setWeight] = useState<string>("");
+  const [age, setAge] = useState<string>("");
+  const [height, setHeight] = useState<string>("");
+  const [waterIntake, setWaterIntake] = useState<string>("");
+  const [dailyIntake, setDailyIntake] = useState<string>("");
+  const [drankToday, setDrankToday] = useState<string>("");
+  const [waterData, setWaterData] = useState<WaterData[]>([]);
+
+  // Load data from AsyncStorage when the component mounts
+  useEffect(() => {
+    const loadFromStorage = async () => {
+      const data = await AsyncStorage.getItem("waterData");
+      if (data) {
+        const parsedData: WaterData[] = JSON.parse(data);
+        setWaterData(parsedData);
+        const latestEntry = parsedData[parsedData.length - 1];
+        setSelectedDate(new Date(latestEntry.date));
+        setWeight(latestEntry.weight);
+        setHeight(latestEntry.height);
+        setAge(latestEntry.age);
+        setDailyIntake(latestEntry.dailyIntake);
+        setDrankToday(latestEntry.drankToday);
+        const result = calculateWaterIntake(
+          Number(latestEntry.weight),
+          Number(latestEntry.height),
+          Number(latestEntry.age)
+        );
+        setWaterIntake(result.toFixed(2));
+      }
+    };
+    loadFromStorage();
+  }, []);
+
+  // Save data to AsyncStorage when user inputs are changed
+  useEffect(() => {
+    const saveToStorage = async () => {
+      const updatedData = waterData.map((data) =>
+        data.date === selectedDate.toISOString()
+          ? {
+              date: selectedDate.toISOString(),
+              weight,
+              height,
+              age,
+              dailyIntake,
+              drankToday,
+            }
+          : data
+      );
+      await AsyncStorage.setItem("waterData", JSON.stringify(updatedData));
+      setWaterData(updatedData);
+    };
+    saveToStorage();
+  }, [weight, height, age, dailyIntake, drankToday, selectedDate]);
+
+  const calculateWaterIntake = (
+    weight: number,
+    height: number,
+    age: number
+  ) => {
+    // Formula for calculating recommended daily water intake based on weight, height, and age
+    // This is just an example, the actual formula may vary based on the source and factors considered
+    const waterIntake =
+      (weight / 2.2) * (30 / 1000) + (height / 100) * 10 + (age / 100) * 5;
+    return waterIntake;
+  };
+
+  const handleCalculate = () => {
+    const result = calculateWaterIntake(
+      Number(weight),
+      Number(height),
+      Number(age)
+    );
+    setWaterIntake(result.toFixed(2));
+  };
+
+  const handleDrankToday = (value: string) => {
+    setDrankToday(value);
+  };
+
+  const handleDateChange = (date: Date) => {
+    setSelectedDate(date);
+    const selectedEntry = waterData.find(
+      (data) => data.date === date.toISOString()
+    );
+    if (selectedEntry) {
+      setWeight(selectedEntry.weight);
+      setHeight(selectedEntry.height);
+      setAge(selectedEntry.age);
+      setDailyIntake(selectedEntry.dailyIntake);
+      setDrankToday(selectedEntry.drankToday);
+    } else {
+      setWeight("");
+      setHeight("");
+      setAge("");
+      setDailyIntake("");
+      setDrankToday("");
+    }
+  };
+
+  const handleDateSelect = (date: Date) => {
+    setSelectedDate(date);
+    const selectedEntry = waterData.find(
+      (data) => data.date === date.toISOString()
+    );
+    if (selectedEntry) {
+      setWeight(selectedEntry.weight);
+      setHeight(selectedEntry.height);
+      setAge(selectedEntry.age);
+      setDailyIntake(selectedEntry.dailyIntake);
+      setDrankToday(selectedEntry.drankToday);
+    } else {
+      setWeight("");
+      setHeight("");
+      setAge("");
+      setDailyIntake("");
+      setDrankToday("");
+      const result = calculateWaterIntake(
+        Number(weight),
+        Number(height),
+        Number(age)
+      );
+      setWaterIntake(result.toFixed(2));
+    }
+  };
+
+  return (
+    <View>
+      <Text>Select date:</Text>
+      {/* <DatePickerIOSComponent
+        date={selectedDate}
+        onDateChange={handleDateSelect}
+      /> */}
+      <Text>Weight (kg):</Text>
+      <TextInput
+        keyboardType="numeric"
+        value={weight}
+        onChangeText={setWeight}
+        placeholder="Enter weight"
+      />
+      <Text>Height (cm):</Text>
+      <TextInput
+        keyboardType="numeric"
+        value={height}
+        onChangeText={setHeight}
+        placeholder="Enter height"
+      />
+      <Text>Age (years):</Text>
+      <TextInput
+        keyboardType="numeric"
+        value={age}
+        onChangeText={setAge}
+        placeholder="Enter age"
+      />
+      <Button title="Calculate" onPress={handleCalculate} />
+      <Text>Daily water intake (L): {waterIntake}</Text>
+      <Text>How much did you drink today (L):</Text>
+      <TextInput
+        keyboardType="numeric"
+        value={drankToday}
+        onChangeText={handleDrankToday}
+        placeholder="Enter amount"
+      />
+      <Text>
+        {drankToday && `You drank ${drankToday}L today. `}
+        {dailyIntake && `Your daily intake is ${dailyIntake}L. `}
+        {drankToday && dailyIntake && Number(drankToday) >= Number(dailyIntake)
+          ? "Good job!"
+          : "You need to drink more."}
+      </Text>
+    </View>
+  );
+};
+
+export default WaterIntakeCalculator;
--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -1,97 +1,19 @@
-import {
-  Dimensions,
-  ScrollView,
-  StyleSheet,
-  TextInput,
-  TouchableOpacity,
-} from "react-native";
-import EditScreenInfo from "../../components/EditScreenInfo";
-import { Text, View } from "../../components/Themed";
+import { Dimensions, ScrollView, StyleSheet } from "react-native";
 import { RootTabScreenProps } from "../../types";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import React, { useState } from "react";
+import React from "react";
 import Wave from "../../components/Wave";
+import WaterIntakeCalculator from "../../components/WaterIntakeCalculator";
 
-interface Consumption {
-  water: string;
-  time: string;
-}
 const myProgress = 0 / 450;
 
 const screenHeight = Dimensions.get("screen").height;
 export const DAILYPROGRESS = myProgress * screenHeight;
 
 const Home = ({ navigation }: RootTabScreenProps<"Home">) => {
-  const [water, setWater] = useState<string>("");
-  const [time, setTime] = useState<string>("");
-  const [data, setData] = useState<Consumption[]>([]);
-
-  const handleSave = async () => {
-    try {
-      const consumption: Consumption = { water, time };
-      const newData: Consumption[] = [...data, consumption];
-      await AsyncStorage.setItem("waterConsumption", JSON.stringify(newData));
-      setData(newData);
-      console.log(`Saved water consumption data: ${JSON.stringify(newData)}`);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleLoad = async () => {
-    try {
-      const data = await AsyncStorage.getItem("waterConsumption");
-      if (data !== null) {
-        setData(JSON.parse(data));
-        console.log(`Loaded water consumption data: ${data}`);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <ScrollView style={styles.container}>
+      <WaterIntakeCalculator />
       <Wave />
-      {/* <Text style={styles.title}>Water Tracker</Text>
-
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => setWater(text)}
-        value={water}
-        placeholder="Enter water consumption (in oz)"
-        keyboardType="numeric"
-      />
-
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => setTime(text)}
-        value={time}
-        placeholder="Enter time of day (e.g. morning)"
-      />
-
-      <TouchableOpacity style={styles.button} onPress={handleSave}>
-        <Text style={styles.buttonText}>Save</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity style={styles.button} onPress={handleLoad}>
-        <Text style={styles.buttonText}>Load</Text>
-      </TouchableOpacity>
-
-      <Text style={styles.info}>
-        You drank {water} oz of water at {time}.
-      </Text>
-
-      {data.length > 0 && (
-        <View>
-          <Text style={styles.dataTitle}>Water Consumption Data</Text>
-          {data.map((item, index) => (
-            <Text key={index} style={styles.dataItem}>
-              {item.water} oz in the {item.time}
-            </Text>
-          ))}
-        </View>
-      )} */}
     </ScrollView>
   );
 };
